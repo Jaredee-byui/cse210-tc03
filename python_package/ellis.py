bc568dd1d776403663e4beb2430eca9a0a723715
Jaredee-byui <admin@Admins-MacBook-Pro.local>
2021-10-01 10:48:51 -0600

    Added the jared python file

---
+++ python_package/ellis.py
@@ -0,0 +1,2 @@
+def ellis_greeting():
+    print("Hello from Jared Ellis! ")
--- python_package/main.py
+++ python_package/main.py
@@ -1,1 +1,4 @@
+#Please place import commands up here
+import ellis
 print("Welcome to our collaborative program!")
+ellis.ellis_greeting()
